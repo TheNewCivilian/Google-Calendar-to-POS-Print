f3801d87f884cc8014d6ce8633c2fdc9b7290462
TheNewCivilian <superregmail@gmx.de>
2017-10-29 23:26:00 +0100

    0.1.1 Small Update
    Printing only category headlines if necessary

--- src/main.py
+++ src/main.py
@@ -21,7 +21,7 @@ for item in taskbuffer:
         #print "->" + item
         #Printer.text(item + " ")
         title += str(item + " ")
-        print title
+        #print title
     else:
         if ':' not in item and dayly is True:
             if '\n' not in item:
@@ -31,7 +31,7 @@ for item in taskbuffer:
                 dayly_entry = dayly_entry.replace('\n','')
                 dayly_entry_list.append(dayly_entry)
                 dayly_entry = ""
-                print dayly_entry_list
+                #print dayly_entry_list
         else:
             dayly = False
             if '\n' and ':' not in item:
@@ -42,17 +42,19 @@ for item in taskbuffer:
                     entry_list.append(entry)
                 entry_list.append(item)
                 entry = ""
-                print entry_list
+                #print entry_list
 Printer = Usb(0x0416,0x5011)
 Printer.set(align=u'center', font=u'a', width=2, height=2, density=9, invert=False, smooth=True, flip=False)
 Printer.text(title+"\n")
 Printer.set(align=u'center', font=u'a', width=1, height=1, density=9, invert=False, smooth=True, flip=False)
-Printer.text("\n------- Heute -------\n\n")
+if len(dayly_entry_list) > 0:
+    Printer.text("\n------- Heute -------\n\n")
 Printer.set(align=u'right', font=u'a', width=1, height=1, density=9, invert=False, smooth=True, flip=False)
 for item in dayly_entry_list:
     Printer.text('{:3s}  {:>27s}'.format( "[ ]", item) + "\n\n")
 Printer.set(align=u'center', font=u'a', width=1, height=1, density=9, invert=False, smooth=True, flip=False)
-Printer.text("\n------- Tasks -------\n\n")
+if len(entry_list) > 0:
+    Printer.text("\n------- Tasks -------\n\n")
 Printer.set(align=u'right', font=u'a', width=1, height=1, density=9, invert=False, smooth=True, flip=False)
 for item in entry_list:
     if entry_list.index(item)%2 == 0 and entry_list.index(item)+2 <len(entry_list):
